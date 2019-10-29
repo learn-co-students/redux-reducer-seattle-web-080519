6d5f05a825ff3390f91f4897a4451fef5fda16b4
zozep <jpark041@gmail.com>
2019-10-29 13:40:00 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,24 @@
-export function manageFriends(state, action){
+import { setupMaster } from "cluster";
+
+export function manageFriends(state = { friends: [] }, action) {
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return { ...state, friends: [...state.friends, action.friend] };
+
+    case "REMOVE_FRIEND":
+      const removealIndex = state.friends.findIndex(
+        friend => friend.id == action.id
+      );
+
+      return {
+        ...state,
+        friends: [
+          ...state.friends.slice(0, removealIndex),
+          ...state.friends.slice(removealIndex + 1)
+        ]
+      };
+
+    default:
+      return state;
+  }
 }
