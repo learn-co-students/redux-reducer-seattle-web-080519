38a94965ba5dee3a95d8accf5c4d1447055be074
Catherine Liahn Yang <liahnee@gmail.com>
2019-11-01 11:11:06 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -2,11 +2,11 @@ export function manageFriends(state = { friends: [] }, action){
 
     switch(action.type) {
         case "ADD_FRIEND":
-            return { ...state, friends: [...state.friends, action.friend] };
+            return { ...state, friends: [...state.friends, action.friend ] };
         case "REMOVE_FRIEND":
             const i = state.friends.findIndex( f => f.id === action.id)
-            return { ...state, friends: [state.friends.slice(0, i), state.friends.slice(i + 1)] };
+            return { ...state, friends: [ ...state.friends.slice(0, i), ...state.friends.slice(i + 1)] };
         default:
-            state;
+            return state;
     };
 };
